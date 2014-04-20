7a517664d847802e8194996f53153087f3e328b9
George <twgeolo@gmail.com>
2014-04-20 15:52:49 -0400

    Initial Commit

---
+++ LiveTranslate/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  LiveTranslate
+//
+//  Created by George Lo on 4/20/14.
+//  Copyright (c) 2014 George Lo & Krishnabh Medhi. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ LiveTranslate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LiveTranslate
+//
+//  Created by George Lo on 4/20/14.
+//  Copyright (c) 2014 George Lo & Krishnabh Medhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
